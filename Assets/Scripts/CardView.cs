9c8c3483c9ea336f1c4f3696d54105e359e2f3cc
lsxpybugMaster <1275425660@qq.com>
2025-09-10 11:36:34 +0800

    2025-9-9 初始化卡面脚本

---
+++ Assets/Scripts/CardView.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text title;
+    [SerializeField] private TMP_Text description;
+    [SerializeField] private TMP_Text mana;
+    [SerializeField] private SpriteRenderer sr;
+    [SerializeField] private GameObject wrapper;
+}
