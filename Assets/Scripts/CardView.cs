c0177d30f618f92462d142b6ca2c3dbad4860140
lsxpybugMaster <1275425660@qq.com>
2025-09-11 21:04:52 +0800

    2025-9-11 卡牌选择时展示效果

--- Assets/Scripts/CardView.cs
+++ Assets/Scripts/CardView.cs
@@ -17,6 +17,10 @@ public class CardView : MonoBehaviour
 
     public Card Card { get; private set; }
 
+    /// <summary>
+    /// 根据卡牌数据更新信息
+    /// </summary>
+    /// <param name="card">卡牌数据</param>
     public void Setup(Card card)
     {
         Card = card;
@@ -25,4 +29,19 @@ public class CardView : MonoBehaviour
         mana.text = card.Mana.ToString();
         imageSR.sprite = card.Image;
     }
+
+    //注意要求有碰撞体才能运行
+    void OnMouseEnter()
+    {
+        wrapper.SetActive(false);
+        Vector3 pos = new(transform.position.x, -2, 0);
+        CardViewHoverSystem.Instance.Show(Card, pos);
+    }
+
+    void OnMouseExit()
+    {
+        CardViewHoverSystem.Instance.Hide();
+        wrapper.SetActive(true);
+    }
+
 }
---
+++ Assets/Scripts/System/CardViewHoverSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardViewHoverSystem : Singleton<CardViewHoverSystem>
+{
+    //专门用于展示卡牌信息的卡牌
+    [SerializeField] private CardView cardViewHover;
+
+    public void Show(Card card, Vector3 position)
+    {
+        cardViewHover.gameObject.SetActive(true);
+        //根据数据更新卡牌信息
+        cardViewHover.Setup(card);
+        cardViewHover.transform.position = position;
+    }
+
+    public void Hide()
+    {
+        cardViewHover.gameObject.SetActive(false);
+    }
+}
