bb8965c3969bd089f5cdd9fb1a6a42eea35bab72
lsxpybugMaster <1275425660@qq.com>
2025-10-03 23:45:12 +0800

    2025-10-3 状态UI效果

---
+++ Assets/Scripts/UI/StatusEffectsUI.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class StatusEffectsUI : MonoBehaviour
+{
+    [SerializeField] private StatusEffectUI statusEffectUIPrefab;
+
+    [SerializeField] private Sprite armorSprite, burnSprite;
+
+    //UI对象字典
+    private Dictionary<StatusEffectType, StatusEffectUI> statusEffectUIs = new();
+
+    /// <summary>
+    /// 根据状态层数及时更新UI图标
+    /// </summary>
+    public void UpdateStatusEffectUI(StatusEffectType statusEffectType, int stackCount)
+    {
+        if (stackCount == 0)
+        {
+            if (statusEffectUIs.ContainsKey(statusEffectType))
+            {
+                StatusEffectUI statusEffectUI = statusEffectUIs[statusEffectType];
+                statusEffectUIs.Remove(statusEffectType);
+                Destroy(statusEffectUI.gameObject);
+            }
+        }
+        else
+        {
+            if (!statusEffectUIs.ContainsKey(statusEffectType))
+            {
+                StatusEffectUI statusEffectUI = Instantiate(statusEffectUIPrefab, transform);
+                statusEffectUIs.Add(statusEffectType, statusEffectUI);
+            }
+            Sprite sprite = GetSpriteByType(statusEffectType);
+            statusEffectUIs[statusEffectType].Set(sprite, stackCount);
+        }
+    }
+
+    //根据状态类型返回对应sprite
+    private Sprite GetSpriteByType(StatusEffectType statusEffectType)
+    {
+        return statusEffectType switch
+        {
+            StatusEffectType.AMROR => armorSprite,
+            StatusEffectType.BURN => burnSprite,
+            _ => null,
+        };
+    }
+}
