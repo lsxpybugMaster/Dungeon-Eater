f77d4f21f694c6e5d746be5fad33949f2e1c4edc
lsxpybugMaster <1275425660@qq.com>
2025-10-06 23:50:09 +0800

    2025-10-6 初步尝试跨场景持久化

---
+++ Assets/Scripts/General/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public enum Scene
+{
+    BATTLE
+}
+/// <summary>
+/// 核心系统,跨场景而存在
+/// </summary>
+public class GameManager : PersistentSingleton<GameManager>
+{
+    private int state = 0;
+
+    public static event Action OnReturnToMenu;
+
+    private void Update()
+    {
+        if (state == 1 && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(1);
+            state = 0;
+        }
+    }
+
+    public void ToBattle()
+    {
+        SceneManager.LoadScene((int)Scene.BATTLE);
+        state = 1;
+    }
+
+
+}
