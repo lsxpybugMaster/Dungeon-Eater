8816aa2a90df882aee0c5814bff1bd02561407f7
lsxpybugMaster <1275425660@qq.com>
2025-10-16 23:20:49 +0800

    2025-10-16 数据更新引发事件,更新UI

--- Assets/Scripts/States/HeroState.cs
+++ Assets/Scripts/States/HeroState.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
 /// 跨场景持久化的数据, 最小化维护开销
+/// 即实际上的玩家全局数据
 /// </summary>
 [System.Serializable]
 public class HeroState
@@ -11,25 +13,33 @@ public class HeroState
     //DISCUSS: 为了完全封装HeroData,我们使用HeroState管理HeroData的不变数据部分,确保其他系统仅知道HeroData存在
     public HeroData BaseData {  get; private set; }
 
+    //------------------------数据更新事件---------------------------
+    public event Action<int> OnDeckSizeChanged;
+
     //------------------------动态数据---------------------------
     public int MaxHealth { get; private set; }
     public int CurrentHealth { get; private set; }
+
+    /// <summary>
+    /// 玩家局外卡组信息在此
+    /// </summary>
     public List<CardData> Deck { get; private set; }
 
     //------------------------持久化数据---------------------------
     public Sprite HeroSprite => BaseData.Image;
 
+    public int DeckSize => Deck.Count;
+
 
     //仅初始化一次初始数据
-    public void Init(HeroData data)
+    public void Init(HeroData heroData)
     {
-        BaseData = data;
+        BaseData = heroData;
 
-        MaxHealth = data.Health;
-        CurrentHealth = data.Health;
-        Deck = data.Deck;
+        MaxHealth = heroData.Health;
+        CurrentHealth = heroData.Health;
+        Deck = heroData.Deck;
     }
-
 
     //及时接受更新的临时数据
     public void Save(int currentHealth, int maxHealth)
@@ -37,4 +47,23 @@ public class HeroState
         MaxHealth = maxHealth;
         CurrentHealth = currentHealth;
     }
+
+
+    /// <summary>
+    /// 向牌组添加卡牌
+    /// </summary>
+    public void AddCardToDeck(CardData cardData)
+    {
+        OnDeckSizeChanged?.Invoke(DeckSize);
+    }
+
+
+    /// <summary>
+    /// 向牌组删除卡牌
+    /// </summary>
+    public void RemoveCardFromDeck(CardData cardData)
+    {
+        OnDeckSizeChanged?.Invoke(DeckSize);
+    }
+
 }
--- Assets/Scripts/UI/GlobalUI.cs
+++ Assets/Scripts/UI/GlobalUI.cs
@@ -10,7 +10,7 @@ public class GlobalUI : PersistentSingleton<GlobalUI>
 {
     [SerializeField] private CanvasGroup canvasGroup;
     [SerializeField] private TMP_Text heroHpTMP;
-    
+    [SerializeField] private TMP_Text deckSizeTMP;
 
     private void Start()
     {
@@ -23,11 +23,29 @@ public class GlobalUI : PersistentSingleton<GlobalUI>
     public void Setup(HeroState heroState)
     {
         UpdateHeroHp(heroState.CurrentHealth, heroState.MaxHealth);
+        UpdateDeckSize(heroState.DeckSize);
+
+        SubscribeEvent(heroState);
     }
 
 
+    //订阅事件统一写在这里
+    private void SubscribeEvent(HeroState heroState)
+    {
+        // 防御性注册
+        heroState.OnDeckSizeChanged -= UpdateDeckSize;
+        heroState.OnDeckSizeChanged += UpdateDeckSize;
+    }
+
+    //TODO: 将生命更新后的对应逻辑挂载到ActionSystem中
     public void UpdateHeroHp(int hpAmount, int maxHpAmount)
     {
         heroHpTMP.text = hpAmount.ToString() + "/" + maxHpAmount.ToString();
+    }
+
+
+    public void UpdateDeckSize(int size)
+    {
+        deckSizeTMP.text = size.ToString();
     }
 }
