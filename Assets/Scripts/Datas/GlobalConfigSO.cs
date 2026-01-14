69d6d023a5a2b2df5aa026ff650c4155cff10ff2
lsxpybugMaster <1275425660@qq.com>
2026-01-14 21:12:53 +0800

    2026-1-14 SO数据分离初步完成

---
+++ Assets/Scripts/Datas/GlobalConfigSO.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/GlobalConfigSO")]
+public class GlobalConfigSO : ScriptableObject
+{
+    [Header("全局信息")]
+    public int seed; //随机数种子
+
+    [Header("战斗数值相关系统")]
+    public int difficultScore;
+
+    [Header("卡牌系统相关参数")]
+    //卡牌大小
+    [Range(0, 2f)] public float cardSize;
+    //卡牌作为动画展示时的大小
+    [Range(0, 2f)] public float cardShowSize;
+
+    [Header("卡牌动画相关参数")]
+    //展示卡牌对象的时间
+    public float freezeTime;
+    //移动卡牌对象动画的时间
+    public float moveTime;
+    //卡牌缩放/膨胀所需的时间
+    public float scaleTime;
+
+    [Header("效果系统相关参数")]
+    //显示VFX的时间
+    public float effectTime;
+
+    [Header("战斗动画相关参数")]
+    public float attackTime;
+
+    [Header("UI动画相关参数")]
+    public float showCardTime;
+
+    [Header("逻辑显示相关参数")]
+    public float logicBetweentime; //如击败全部敌人后显示胜利的时间间隔
+}
--- Assets/Scripts/General/Config.cs
+++ Assets/Scripts/General/Config.cs
@@ -7,24 +7,39 @@ using UnityEngine;
 /// </summary>
 public class Config : PersistentSingleton<Config>
 {
-    //TODO: 这你能忍住不改成SO？
-    [Header("全局信息")]
-    [SerializeField] private int seed; //随机数种子
-    public int Seed 
+    [SerializeField] private GlobalConfigSO so;
+
+    private new void Awake()
     {
-        get
-        {
-            Debug.Log($"seed = {seed}");
-            return seed == 0 ? UnityEngine.Random.Range(0, int.MaxValue) : seed;
-        }
+        base.Awake();
+
+        CopyData(so);
     }
+
+    private void CopyData(GlobalConfigSO so)
+    {
+        seed = so.seed == 0 ? Random.Range(0, int.MaxValue) : seed;
+        difficultScore = so.difficultScore;
+        cardSize = so.cardSize;
+        cardShowSize = so.cardShowSize;
+        freezeTime = so.freezeTime;
+        moveTime = so.moveTime;
+        scaleTime = so.scaleTime;
+        effectTime = so.effectTime;
+        attackTime = so.attackTime;
+        showCardTime = so.showCardTime;
+        logicBetweentime = so.logicBetweentime;  //如击败全部敌人后显示胜利的时间间隔
+    }
+
+    private int seed; //随机数种子
+    public int Seed { get { return seed; } }    
 
     [Header("战斗数值相关系统")]
     public int difficultScore;
 
     [Header("卡牌系统相关参数")]
     //卡牌大小
-    [Range(0, 2f)] public float cardSize; 
+    [Range(0, 2f)] public float cardSize;
     //卡牌作为动画展示时的大小
     [Range(0, 2f)] public float cardShowSize;
 
@@ -48,5 +63,50 @@ public class Config : PersistentSingleton<Config>
 
     [Header("逻辑显示相关参数")]
     public float logicBetweentime; //如击败全部敌人后显示胜利的时间间隔
+
+
+
+
+
+    //[Header("全局信息")]
+    //[SerializeField] private int seed; //随机数种子
+    //public int Seed 
+    //{
+    //    get
+    //    {
+    //        Debug.Log($"seed = {seed}");
+    //        return seed == 0 ? UnityEngine.Random.Range(0, int.MaxValue) : seed;
+    //    }
+    //}
+
+    //[Header("战斗数值相关系统")]
+    //public int difficultScore;
+
+    //[Header("卡牌系统相关参数")]
+    ////卡牌大小
+    //[Range(0, 2f)] public float cardSize; 
+    ////卡牌作为动画展示时的大小
+    //[Range(0, 2f)] public float cardShowSize;
+
+    //[Header("卡牌动画相关参数")]
+    ////展示卡牌对象的时间
+    //public float freezeTime;
+    ////移动卡牌对象动画的时间
+    //public float moveTime;
+    ////卡牌缩放/膨胀所需的时间
+    //public float scaleTime;
+
+    //[Header("效果系统相关参数")]
+    ////显示VFX的时间
+    //public float effectTime;
+
+    //[Header("战斗动画相关参数")]
+    //public float attackTime;
+
+    //[Header("UI动画相关参数")]
+    //public float showCardTime;
+
+    //[Header("逻辑显示相关参数")]
+    //public float logicBetweentime; //如击败全部敌人后显示胜利的时间间隔
 
 }
