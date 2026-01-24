de1ddcf35030ce2c1feed609cf0ba6a62e99dc01
lsxpybugMaster <1275425660@qq.com>
2026-01-24 20:56:47 +0800

    2026-1-24 完成圣骑士敌人的设计,准备设计添加多张卡牌的功能

--- Assets/Scripts/AI/Conditions/IntendCondition.cs
+++ Assets/Scripts/AI/Conditions/IntendCondition.cs
@@ -9,6 +9,7 @@ public enum Relation
     greater_equal,
     equal,
     if_mod, //回合数模该值为0时执行
+    start_and_if_mod //初始执行 + 回合数模该值为0时执行
 }
 
 //决定敌人意图的条件
@@ -18,12 +19,13 @@ public abstract class IntendCondition
     //static的字典存储辅助类
     protected static readonly Dictionary<Relation, Func<int, int, bool>> Compare = new()
     {
-        { Relation.less,         (a, b) => a < b },
-        { Relation.equal,        (a, b) => a == b },
-        { Relation.greater,      (a, b) => a > b },
-        { Relation.less_equal,   (a, b) => a <= b},
-        { Relation.greater_equal,(a, b) => a >= b },
-        { Relation.if_mod,       (a, b) => a % b == 0}
+        { Relation.less,             (a, b) => a < b },
+        { Relation.equal,            (a, b) => a == b },
+        { Relation.greater,          (a, b) => a > b },
+        { Relation.less_equal,       (a, b) => a <= b},
+        { Relation.greater_equal,    (a, b) => a >= b },
+        { Relation.if_mod,           (a, b) => a % b == 0},
+        { Relation.start_and_if_mod, (a, b) => a == 1 || a % b == 0 }
     };
 
 
